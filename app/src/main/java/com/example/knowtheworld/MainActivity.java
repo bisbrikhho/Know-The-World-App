b1007698df821f0e7f564e023cc09d172ce8ed80
bisbrikhho <polasitsmy@gmail.com>
2023-01-10 02:16:24 +0600

    know the world app

--- app/src/main/java/com/example/knowtheworld/MainActivity.java
+++ app/src/main/java/com/example/knowtheworld/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.knowtheworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layout = findViewById(R.id.layout);
+        AnimationDrawable drawable = (AnimationDrawable) layout.getBackground();
+        drawable.setEnterFadeDuration(2500);
+        drawable.setExitFadeDuration(5000);
+        drawable.start();
+
     }
 }
